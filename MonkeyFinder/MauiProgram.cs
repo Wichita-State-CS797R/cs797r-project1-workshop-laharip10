ca94bb70e76ba831203ecddcb1983c73380b7d3f
laharip10 <86415826+laharip10@users.noreply.github.com>
2023-09-27 22:05:22 -0500

    introduced maps in application.

--- MonkeyFinder/MauiProgram.cs
+++ MonkeyFinder/MauiProgram.cs
@@ -17,7 +17,11 @@ public static class MauiProgram
 			});
 
 
-			
+		
+    	builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+		builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+		builder.Services.AddSingleton<IMap>(Map.Default);
+
 		builder.Services.AddSingleton<MonkeyService>();
 		builder.Services.AddSingleton<MonkeysViewModel>();
 		builder.Services.AddSingleton<MainPage>();
